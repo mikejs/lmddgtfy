40878916295b5e34cd7d7ca78f605af52695934a
Michael Stephens <me@mikej.st>
2010-05-09 16:52:49 -0400

    store some more stats in redis

--- search/views.py
+++ search/views.py
@@ -10,8 +10,12 @@ def index(request):
         return render_to_response('index.html')
 
     type = request.GET.get('v', '')
+    quoted_query = urllib.quote(query).replace(':', '_')
 
     r.incr("total_searches")
-    r.incr("q:%s:views" % urllib.quote(query))
+    r.incr("q:%s:views" % quoted_query)
+    r.incr("q:%s:%s:views" % (quoted_query, type))
+    r.sadd("queries", quoted_query)
+    r.zincr("zqueries", quoted_query)
     return render_to_response('lmddgtfy.html', {'query': query,
                                                 'type': type})
